3663f21e8701143698b190af08fbda32cbd6a173
Daniel <danieljackson104@googlemail.com>
2024-10-04 11:35:31 +0100

    adding project stucture

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <NavBar />
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}']
+export const theme = {
+  extend: {}
+}
+export const plugins = []
