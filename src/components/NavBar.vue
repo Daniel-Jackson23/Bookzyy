1cdb34346a5799220d34abf0c8f90aedad32e989
Daniel <danieljackson104@googlemail.com>
2024-10-22 11:01:41 +0100

    Adding new home page title and text.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -39,7 +39,7 @@
                 v-if="user"
                 to="/calender"
                 class="text-gray-200 hover:text-gray-400 text font-semibold py-1 px-3"
-                >Calender</router-link
+                >Book</router-link
               >
             </li>
             <li class="hidden sm:flex">
@@ -73,6 +73,31 @@
                 class="text-gray-200 hover:text-gray-400 text font-semibold py-1 px-3"
                 >logout</router-link
               >
+            </li>
+            <li class="hidden sm:flex">
+              <router-link>
+                <button
+                  type="button"
+                  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Pricing & Free Trail
+                  <svg
+                    class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </button>
+              </router-link>
             </li>
           </ul>
           <ul class="flex sm:hidden" @click="showMenu = !showMenu">
@@ -127,7 +152,7 @@
                       v-if="user"
                       to="/calender"
                       class="flex flex-1 text-lg text-gray-700 hover:bg-gray-200 font-semibold px-8 py-4"
-                      >Calender</router-link
+                      >Book</router-link
                     >
                   </li>
                   <li class="flex">
@@ -161,6 +186,32 @@
                       class="flex flex-1 text-lg text-gray-700 hover:bg-gray-200 font-semibold px-8 py-4"
                       >logout</router-link
                     >
+                  </li>
+
+                  <li class="flex justify-center p-7">
+                    <router-link>
+                      <button
+                        type="button"
+                        class="p-10 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                      >
+                        Pricing & Free Trail
+                        <svg
+                          class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 14 10"
+                        >
+                          <path
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M1 5h12m0 0L9 1m4 4L9 9"
+                          />
+                        </svg>
+                      </button>
+                    </router-link>
                   </li>
                 </ul>
               </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,20 @@
 <script setup></script>
 
 <template>
-  <main>welcome</main>
+  <div class="container mx-auto px-5 py-5">
+    <div class="">
+      <h1
+        class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-gray-400 md:text-5xl lg:text-6xl"
+      >
+        <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400"
+          >Bookzyy Booking</span
+        >
+        Staff Holiday System.
+      </h1>
+      <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+        Bookzyy is a holiday management system tailored for small to medium sized busniesses,
+        helping you manage holidays, personal leave and maternity leave...
+      </p>
+    </div>
+  </div>
 </template>
