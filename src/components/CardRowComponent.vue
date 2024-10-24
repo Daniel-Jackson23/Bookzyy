5c7f1e048ee647718a075dbc7f0545ae9969b0ad
Daniel <danieljackson104@googlemail.com>
2024-10-24 20:18:17 +0100

    Adding env to gitignore file

---
+++ src/components/CardRowComponent.vue
@@ -0,0 +1,17 @@
+<script>
+import CardData from '../assets/Data/CardData.json'
+
+export default {
+  data() {
+    return {
+      products: CardData
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <p v-for="product in products" :key="product.id">{{ product.name }}</p>
+  </div>
+</template>
