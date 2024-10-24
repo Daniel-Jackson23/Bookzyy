ea8befb599df8190d6f539f5265e88a3adea648f
Daniel <danieljackson104@googlemail.com>
2024-10-24 20:17:42 +0100

    Adding env to gitignore file

--- src/views/CalenderView.vue
+++ src/views/CalenderView.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="flex container mx-auto px-5 py-5">
-    <VCalender />
-    <VDatePicker v-model="date" />
+    
+    
+    
+    <!-- <VCalender />
+    <VDatePicker v-model="date" /> -->
   </div>
 </template>
 
