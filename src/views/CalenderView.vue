5df3037ae5020322e7d34e49be24c90fb8dfbb0b
Daniel <danieljackson104@googlemail.com>
2024-10-31 16:07:17 +0000

    adding anything that had changed

--- src/views/CalenderView.vue
+++ src/views/CalenderView.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="flex container mx-auto px-5 py-5">
-    
-    
-    
-    <!-- <VCalender />
-    <VDatePicker v-model="date" /> -->
+    <VCalender />
+    <VDatePicker v-model="date" />
   </div>
 </template>
 
